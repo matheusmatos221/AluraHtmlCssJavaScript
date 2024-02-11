c081a3dfc00503d89f13be726e811e36d5bc07ea
Matheus Matos <matheusmatos.221@hotmail.com>
2024-02-11 15:24:59 -0300

    Add javascript pause and play functions and beep plays

--- script.js
+++ script.js
@@ -5,16 +5,25 @@ const longBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBtn = document.querySelector('#start-pause');
+
+let tempoRestanteEmSegundos = 5;
+let intervaloId = null;
+
+const pauseAudio = new Audio("/AluraCourse/Alura3-ManipulandoElementosNoDOM/Fokus/sons/pause.mp3");
+const playAudio = new Audio("/AluraCourse/Alura3-ManipulandoElementosNoDOM/Fokus/sons/play.wav");
+const audioTempoFinalizado = new Audio("/AluraCourse/Alura3-ManipulandoElementosNoDOM/Fokus/sons/beep.mp3");
 
 
 const switchMusica = document.querySelector("#alternar-musica");
 const musica = new Audio("/AluraCourse/Alura3-ManipulandoElementosNoDOM/Fokus/sons/luna-rise-part-one.mp3");
+
 musica.loop = true;
 
-switchMusica.addEventListener("change", function() {
-    if(musica.paused){
+switchMusica.addEventListener("change", function () {
+    if (musica.paused) {
         musica.play();
-    }else{
+    } else {
         musica.pause();
     }
 })
@@ -67,3 +76,36 @@ function alterarContexto(contexto) {
             break;
     }
 }
+
+function zerarIntervaloId() {
+    clearInterval(intervaloId)
+    intervaloId = null;
+}
+
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        pauseAudio.play();
+        zerarIntervaloId();
+        return
+    }
+    playAudio.play();
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+
+const contagemRegressiva = () => {
+    if (tempoRestanteEmSegundos <= 0) {
+        audioTempoFinalizado.play();
+        zerarIntervaloId();
+        tempoRestanteEmSegundos = 5;
+        alert("Tempo finalizado!");
+        return
+    }
+    tempoRestanteEmSegundos -= 1;
+    console.log('Temporizador: ' + tempoRestanteEmSegundos)
+}
+
+startPauseBtn.addEventListener('click', iniciarOuPausar)
+
+
